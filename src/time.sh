5de356a22a3322c66fcad165f2e04c78ca6a3e97
silentcoder99 <silentcoder99@gmail.com>
2019-04-14 11:07:42 +1000

    Update time.sh to have more parameters

--- src/time.sh
+++ src/time.sh
@@ -1,5 +1,5 @@
-: > $2 ## Clear output file
+: > $3 ## Clear output file
 
 while read -r LINE; do
-    echo "$LINE" | java -cp .:jopt-simple-5.0.2.jar GraphEvalTimed incmat -f assocGraph.csv vert.out edge.out neigh.out misc.out >> $2
-done < $1
+    echo "$LINE" | java -cp .:jopt-simple-5.0.2.jar GraphEvalTimed $1 -f $4 vert.out edge.out neigh.out misc.out >> $3
+done < $2
